148b255a1541fbb04e28e5eca14aacff0d371b34
LeonieRiederer <73276369+LeonieRiederer@users.noreply.github.com>
2022-12-06 12:21:44 +0100

    Add_Animation_Door_Split_Animation

---
+++ Interactive3D/Assets/SampleSceneAssets/Scripts/ActivationInteraction.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class ActivationInteraction : InteractibleObject
+//{
+  // List of GameObject to activate/ deactivate
+  //public GameObject [] objectsToActivate;
+
+  //void Start ()
+  //{
+    //for (int i = 0; i < objectsToActivate.Length; i++)
+     // {
+     //   objectsToActivate [i]. SetActivate(!objectsToActivate[i].activeInHierarchy);
+     // }
+  //}
+
+    //public override void TriggerInteraction()
+   // {
+      //for (int i = 0; i < objectsToActivate.Length; i++)
+      //{
+      //  objectsToActivate [i]. SetActivate(!objectsToActivate[i].activeInHierarchy);
+      //}
+   // }
+//}
