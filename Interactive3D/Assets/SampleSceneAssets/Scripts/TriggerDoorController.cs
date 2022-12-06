582b5361f95ea986ff9e729cf9158468e6a62c5b
LeonieRiederer <73276369+LeonieRiederer@users.noreply.github.com>
2022-12-06 20:56:01 +0100

    Updated_Player

--- Interactive3D/Assets/SampleSceneAssets/Scripts/TriggerDoorController.cs
+++ Interactive3D/Assets/SampleSceneAssets/Scripts/TriggerDoorController.cs
@@ -2,28 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerDoorController : MonoBehaviour
-{
-    [SerializeField] private Animator LoFiDoor = null;
+//public class TriggerDoorController : MonoBehaviour
+//{
+    //[SerializeField] private Animator LoFiDoor = null;
 
-    [SerializeField] private bool openTrigger = false;
-    [SerializeField] private bool closeTrigger = false;
+    //[SerializeField] private bool openTrigger = false;
+    //[SerializeField] private bool closeTrigger = false;
 
-    private void OnTriggerEnter  (Collider other)
-    {
-      if (other.CompareTag("Player"))
-      {
-        if (openTrigger)
-        {
-          LoFiDoor.Play("DoorOpen", 0, 0.0f);
-          gameObject.SetActive(false);
-        }
+    //private void OnTriggerEnter  (Collider other)
+    //{
+      //if (other.CompareTag("Player"))
+      //{
+        //if (openTrigger)
+       // {
+         // LoFiDoor.Play("DoorOpen", 0, 0.0f);
+         // gameObject.SetActive(false);
+       // }
 
-        else if (closeTrigger)
-        {
-          LoFiDoor.Play("DoorClose", 0, 0.0f);
-          gameObject.SetActive(false);
-        }
-      }
-    }
-}
+        //else if (closeTrigger)
+        //{
+         // LoFiDoor.Play("DoorClose", 0, 0.0f);
+          //gameObject.SetActive(false);
+        //}
+      //}
+    //}
+//}
