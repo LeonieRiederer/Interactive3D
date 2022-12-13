5ce17410b048df577d458dc7e8bf405aef12e4b3
LeonieRiederer <73276369+LeonieRiederer@users.noreply.github.com>
2022-12-13 21:17:14 +0100

    save

--- Interactive3D/Assets/SampleSceneAssets/Scripts/GazeController.cs
+++ Interactive3D/Assets/SampleSceneAssets/Scripts/GazeController.cs
@@ -53,9 +53,9 @@ public class GazeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E) && currentGazeObject != null)
+        if(Input.GetKeyDown(KeyCode.F) && currentGazeObject != null)
         {
-          currentGazeObject.TriggerInteraction();
+         currentGazeObject.TriggerInteraction();
         }
     }
 }
